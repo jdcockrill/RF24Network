2a0db11239988b020c210696bfd2ab29e9fc3ec5
jdcockrill <jamie.cockrill@gmail.com>
2013-10-30 10:28:59 +0000

    inital checkin of helloworld_rx example. Makefile doesn't work yet

---
+++ examples/helloworld_rx/helloworld_rx.cpp
@@ -0,0 +1,67 @@
+/*
+ Copyright (C) 2012 James Coliz, Jr. <maniacbug@ymail.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+ */
+
+/**
+ * Simplest possible example of using RF24Network,
+ *
+ * RECEIVER NODE
+ * Listens for messages from the transmitter and prints them out.
+ */
+
+#include "RF24Network.h"
+#include <RF24.h>
+#include <cstdlib>
+#include <iostream>
+
+// nRF24L01(+) radio attached using Getting Started board 
+RF24 radio(9,10,8000000);
+
+// Network uses that radio
+RF24Network network(radio);
+
+// Address of our node
+const uint16_t this_node = 0;
+
+// Address of the other node
+const uint16_t other_node = 1;
+
+// Structure of our payload
+struct payload_t
+{
+  unsigned long ms;
+  unsigned long counter;
+};
+
+int main(int argc, char** argv)
+{
+  printf("RF24Network/examples/helloworld_rx/\n");
+ 
+  radio.begin();
+  network.begin(/*channel*/ 90, /*node address*/ this_node);
+
+  while(1)
+  {
+    // Pump the network regularly
+    network.update();
+
+    // Is there anything ready for us?
+    while ( network.available() )
+    {
+      // If so, grab it and print it out
+      RF24NetworkHeader header;
+      payload_t payload;
+      network.read(header,&payload,sizeof(payload));
+      printf("Received packet #");
+      printf(payload.counter);
+      printf(" at ");
+      printf(payload.ms);
+      printf("\n");
+    }
+  }
+}
+// vim:ai:cin:sts=2 sw=2 ft=cpp
